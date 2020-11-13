0f5a5aba0d8828be3fc1d7d5d42bddb43a7842d2
Scott Leggett <scott@sl.id.au>
2020-11-13 16:30:42 +0800

    Add interactive prompt for PIN/PUK

--- cmd/piv-agent/setup.go
+++ cmd/piv-agent/setup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
@@ -9,19 +10,21 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/go-piv/piv-go/piv"
 	"go.uber.org/zap"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // SetupCmd represents the setup command.
 type SetupCmd struct {
 	Card             string `kong:"help='Specify a smart card device'"`
 	ResetSecurityKey bool   `kong:"help='Overwrite any existing keys'"`
-	PIN              uint64 `kong:"required,help='Set the PIN/PUK of the device (6-8 chars)'"`
+	PIN              uint64 `kong:"help='Set the PIN/PUK of the device (6-8 digits). Will be prompted interactively if not provided.'"`
 	AllTouchPolicies bool   `kong:"default='true',help='Create two additional keys with touch policies always and never (default true)'"`
 }
 
@@ -50,6 +53,29 @@ type sshPubKeySpec struct {
 	serial      uint32
 }
 
+func interactivePIN() (uint64, error) {
+	fmt.Print("Enter a new PIN/PUK (6-8 digits): ")
+	rawPIN, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		return 0, fmt.Errorf("couldn't read PIN/PUK: %w", err)
+	}
+	pin, err := strconv.ParseUint(string(rawPIN), 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid characters: %w", err)
+	}
+	fmt.Print("Repeat PIN/PUK: ")
+	repeat, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		return 0, fmt.Errorf("couldn't read PIN/PUK: %w", err)
+	}
+	if !bytes.Equal(repeat, rawPIN) {
+		return 0, fmt.Errorf("PIN/PUK entries not equal")
+	}
+	return pin, nil
+}
+
 // Run the setup command to configure a security key.
 func (cmd *SetupCmd) Run() error {
 	log, err := zap.NewDevelopment()
@@ -57,8 +83,15 @@ func (cmd *SetupCmd) Run() error {
 		return fmt.Errorf("couldn't init logger: %w", err)
 	}
 	defer log.Sync()
-	if cmd.PIN < 100000 {
-		return fmt.Errorf("invalid PIN, must be > 8 numerals")
+	// if PIN has not been specified, ask interactively
+	if cmd.PIN == 0 {
+		cmd.PIN, err = interactivePIN()
+		if err != nil {
+			return err
+		}
+	}
+	if cmd.PIN < 100000 || cmd.PIN > 99999999 {
+		return fmt.Errorf("invalid PIN, must be 6-8 digits")
 	}
 	k, err := getSecurityKey(cmd.Card)
 	if err != nil {
