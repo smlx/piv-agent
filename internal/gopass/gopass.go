01eec1f20efdcb3fe47fd9b86930624dbe39a49e
Scott Leggett <scott@sl.id.au>
2020-11-21 13:53:04 +0800

    Add some descriptive comments to the server functions

--- internal/gopass/gopass.go
+++ internal/gopass/gopass.go
@@ -22,31 +22,44 @@ func (c *GPCrypto) ListIdentities(ctx context.Context, _ *emptypb.Empty) (*pb.Id
 	return nil, nil
 }
 
+// GenerateIdentity will create a new keypair in batch mode.
 func (c *GPCrypto) GenerateIdentity(ctx context.Context, a *pb.Identity) (*emptypb.Empty, error) {
 	return nil, nil
 }
 
 // Crypto
 
+// Encrypt will encrypt the given content for the recipients.
 func (c *GPCrypto) Encrypt(ctx context.Context, a *pb.EncryptArgs) (*pb.Ciphertext, error) {
 	return nil, nil
 }
+
+// Decrypt will try to decrypt the given data.
 func (c *GPCrypto) Decrypt(ctx context.Context, a *pb.DecryptArgs) (*pb.Cleartext, error) {
 	return nil, nil
 }
 
+// Name returns "piv-agent".
 func (c *GPCrypto) Name(ctx context.Context, _ *emptypb.Empty) (*pb.ServerName, error) {
 	return nil, nil
 }
+
+// Version will return piv-agent version information.
 func (c *GPCrypto) Version(ctx context.Context, _ *emptypb.Empty) (*pb.ServerVersion, error) {
 	return nil, nil
 }
+
+// Initialized always returns nil.
 func (c *GPCrypto) Initialized(ctx context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
 	return nil, nil
 }
+
+// Ext returns the file extension used by this backend: "piv".
 func (c *GPCrypto) Ext(ctx context.Context, _ *emptypb.Empty) (*pb.Extension, error) {
 	return nil, nil
 }
+
+// IDFile returns the name of the recipients file used by this backend: ".piv-id".
 func (c *GPCrypto) IDFile(ctx context.Context, _ *emptypb.Empty) (*pb.IDFileName, error) {
 	return nil, nil
 }
