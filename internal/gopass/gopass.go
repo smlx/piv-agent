7383315821c8fbdd73d1ad3b8b7cff6652732c73
Scott Leggett <scott@sl.id.au>
2020-11-24 23:21:00 +0800

    WIP encrypt

--- internal/gopass/gopass.go
+++ internal/gopass/gopass.go
@@ -10,16 +10,34 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"fmt"
+	"io"
+	"math/big"
 	"time"
 
 	"github.com/aead/ecdh"
 	"github.com/smlx/piv-agent/internal/gopass/pb"
 	"github.com/smlx/piv-agent/internal/token"
+	"github.com/vmihailenco/msgpack/v5"
 	"go.uber.org/zap"
+	"golang.org/x/crypto/nacl/secretbox"
 	"golang.org/x/crypto/scrypt"
 	"golang.org/x/crypto/ssh"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+// ECDSAPubKeyParams contains the parameters for an ECDSA public key.
+type ECDSAPubKeyParams struct {
+	X *big.Int
+	Y *big.Int
+}
+
+// Secret contains the secret with all required parameters for decryption.
+type Secret struct {
+	Ciphertext []byte
+	Nonce      [24]byte
+	KeyType    string
+	PubKey     []byte
+}
 
 // GPCrypto implements the gopass backend crypto interface defined in
 // https://github.com/gopasspw/gopass/blob/master/internal/backend/crypto.go
@@ -63,6 +81,7 @@ func (c *GPCrypto) ListIdentities(ctx context.Context, _ *emptypb.Empty) (*pb.Id
 
 // Encrypt will encrypt the given content for the recipients.
 func (c *GPCrypto) Encrypt(ctx context.Context, a *pb.EncryptArgs) (*pb.Ciphertext, error) {
+	ct := map[string]Secret{}
 	for _, recipient := range a.Recipients {
 		// Unmarshal the public key
 		pubKey, _, _, _, err := ssh.ParseAuthorizedKey(recipient)
@@ -74,34 +93,71 @@ func (c *GPCrypto) Encrypt(ctx context.Context, a *pb.EncryptArgs) (*pb.Cipherte
 		// Perform ECDH to generate a shared secret
 		salt := make([]byte, 20)
 		_, err = rand.Read(salt)
-		var secretKey []byte
 		if err != nil {
 			return nil, fmt.Errorf("couldn't read salt: %w", err)
+		}
+		var secretKey [32]byte
+		es := Secret{
+			KeyType: pubKey.Type(),
 		}
 		switch pubKey.Type() {
 		case "ecdsa-sha2-nistp256":
 			p256 := ecdh.Generic(elliptic.P256())
+			if err = p256.Check(pubKey); err != nil {
+				return nil, fmt.Errorf("public key not on nistp256 curve: %w", err)
+			}
 			privEphemeral, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 			if err != nil {
-				return nil, fmt.Errorf("couldn't generate key: %w", err)
+				return nil, fmt.Errorf("couldn't generate ecdsa key: %w", err)
 			}
-			if err = p256.Check(pubKey); err != nil {
-				return nil, fmt.Errorf("public key not on the curve: %w", err)
+			es.PubKey, err = msgpack.Marshal(&ECDSAPubKeyParams{
+				X: privEphemeral.PublicKey.X,
+				Y: privEphemeral.PublicKey.Y,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("couldn't marshal ecdsa pub key: %w", err)
 			}
-			dhSecret := p256.ComputeSecret(privEphemeral, pubKey)
 			// params from https://blog.filippo.io/the-scrypt-parameters/
 			// Use scrypt as a KDF from the shared secret
-			secretKey, err = scrypt.Key(dhSecret, salt, 1<<20, 8, 1, 32)
+			keyBytes, err := scrypt.Key(
+				p256.ComputeSecret(privEphemeral, pubKey), salt, 1<<20, 8, 1, 32)
+			if err != nil {
+				return nil, fmt.Errorf("scrypt KDF error: %w", err)
+			}
+			copy(secretKey[:], keyBytes)
 		case "ssh-ed25519":
-			_, privKey, err := ed25519.GenerateKey(rand.Reader)
+			c25519 := ecdh.X25519()
+			if err = c25519.Check(pubKey); err != nil {
+				return nil, fmt.Errorf("public key not on ed25519 curve: %w", err)
+			}
+			pubEphemeral, privEphemeral, err := ed25519.GenerateKey(rand.Reader)
+			if err != nil {
+				return nil, fmt.Errorf("couldn't generate ed25519 key: %w", err)
+			}
+			es.PubKey = pubEphemeral
+			keyBytes, err := scrypt.Key(
+				c25519.ComputeSecret(privEphemeral, pubKey), salt, 1<<20, 8, 1, 32)
+			if err != nil {
+				return nil, fmt.Errorf("scrypt KDF error: %w", err)
+			}
+			copy(secretKey[:], keyBytes)
 		default:
 			return nil, fmt.Errorf("invalid recipient keytype: %v", pubKey.Type())
 		}
 		// Generate a nonce
+		if _, err := io.ReadFull(rand.Reader, es.Nonce[:]); err != nil {
+			return nil, fmt.Errorf("couldn't generate nonce: %w", err)
+		}
 		// generate a nacl.secretbox
-		// return the ciphertext, nonce, ephemeral public key
+		es.Ciphertext = secretbox.Seal(es.Nonce[:], a.Plaintext, &es.Nonce, &secretKey)
+		// TODO: figure out the correct data structure for this.
+		// []byte key is not allowed, string is. could convert but messy.
+		// maybe a list is better than a map?
+		ct[recipient] = es
 	}
-	return nil, nil
+	// marshal to yaml
+	// return
+	return ct, nil
 }
 
 // Decrypt will try to decrypt the given data.
