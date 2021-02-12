ab916bc79baf4675d90e1b1effd9d85ad729fbb1
Scott Leggett <scott@sl.id.au>
2021-02-12 14:47:47 +0800

    fix: avoid dropping pinentry errors

--- internal/agent/pinentry.go
+++ internal/agent/pinentry.go
@@ -14,17 +14,32 @@ func pinEntry(sk *token.SecurityKey) func() (string, error) {
 			return "", fmt.Errorf("couldn't get pinentry client: %w", err)
 		}
 		defer p.Close()
-		p.Set("title", "piv-agent PIN Prompt")
+		err = p.Set("title", "piv-agent PIN Prompt")
+		if err != nil {
+			return "", fmt.Errorf("couldn't set title on PIN pinentry: %w", err)
+		}
 		r, err := sk.Key.Retries()
 		if err != nil {
 			return "", fmt.Errorf("couldn't get retries for security key: %w", err)
 		}
-		p.Set("desc",
+		err = p.Set("desc",
 			fmt.Sprintf("serial number: %d, attempts remaining: %d", sk.Serial, r))
-		p.Set("prompt", "Please enter PIN:")
+		if err != nil {
+			return "", fmt.Errorf("couldn't set desc on PIN pinentry: %w", err)
+		}
+		err = p.Set("prompt", "Please enter PIN:")
+		if err != nil {
+			return "", fmt.Errorf("couldn't set prompt on PIN pinentry: %w", err)
+		}
 		// optional PIN cache with yubikey-agent compatibility
-		p.Option("allow-external-password-cache")
-		p.Set("KEYINFO", fmt.Sprintf("--yubikey-id-%d", sk.Serial))
+		err = p.Option("allow-external-password-cache")
+		if err != nil {
+			return "", fmt.Errorf("couldn't set option on PIN pinentry: %w", err)
+		}
+		err = p.Set("KEYINFO", fmt.Sprintf("--yubikey-id-%d", sk.Serial))
+		if err != nil {
+			return "", fmt.Errorf("couldn't set KEYINFO on PIN pinentry: %w", err)
+		}
 		pin, err := p.GetPin()
 		return string(pin), err
 	}
@@ -36,12 +51,32 @@ func getPassphrase(keyPath, fingerprint string) ([]byte, error) {
 		return []byte{}, fmt.Errorf("couldn't get pinentry client: %w", err)
 	}
 	defer p.Close()
-	p.Set("title", "piv-agent Passphrase Prompt")
-	p.Set("prompt", "Please enter passphrase")
-	p.Set("desc", fmt.Sprintf("%s %s %s", keyPath,
-		fingerprint[:25], fingerprint[25:]))
+	err = p.Set("title", "piv-agent Passphrase Prompt")
+	if err != nil {
+		return nil,
+			fmt.Errorf("couldn't set title on passphrase pinentry: %w", err)
+	}
+	err = p.Set("prompt", "Please enter passphrase")
+	if err != nil {
+		return nil,
+			fmt.Errorf("couldn't set prompt on passphrase pinentry: %w", err)
+	}
+	err = p.Set("desc",
+		fmt.Sprintf("%s %s %s", keyPath, fingerprint[:25], fingerprint[25:]))
+	if err != nil {
+		return nil,
+			fmt.Errorf("couldn't set desc on passphrase pinentry: %w", err)
+	}
 	// optional PIN cache
-	p.Option("allow-external-password-cache")
-	p.Set("KEYINFO", fingerprint)
+	err = p.Option("allow-external-password-cache")
+	if err != nil {
+		return nil,
+			fmt.Errorf("couldn't set option on passphrase pinentry: %w", err)
+	}
+	err = p.Set("KEYINFO", fingerprint)
+	if err != nil {
+		return nil,
+			fmt.Errorf("couldn't set KEYINFO on passphrase pinentry: %w", err)
+	}
 	return p.GetPin()
 }
